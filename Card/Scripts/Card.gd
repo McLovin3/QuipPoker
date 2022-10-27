c2f736e74fee34bce5a2f27bcd96347b733fcfbd
Mathieu Ford <mathieu.ford@gmail.com>
2022-10-27 18:23:56 -0400

    Added Kings and Jacks

--- Card/Scripts/Card.gd
+++ Card/Scripts/Card.gd
@@ -1,9 +1,14 @@
 extends Sprite
 
 export var card_value : Texture
+export var card_background : Texture
 
-onready var _card_value : Sprite = $CardValue
+onready var _card_value_sprite : Sprite = $CardValue
+onready var _card_background_sprite : Sprite = $CardBackground
 
 func _ready():
 	if (card_value):
-		_card_value.texture = card_value
+		_card_value_sprite.texture = card_value
+	
+	if (card_background):
+		_card_background_sprite.texture = card_background
