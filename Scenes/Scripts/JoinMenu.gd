b9fe3ff59d5d3a675dfc2fe162d2b79169fddd51
Mathieu Ford <mathieu.ford@gmail.com>
2022-11-04 17:21:59 -0400

    Added JoinMenu

--- Scenes/Scripts/JoinMenu.gd
+++ Scenes/Scripts/JoinMenu.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+onready var _ip_input: ShakingInput = $CanvasLayer/IpInput
+
 
 func _on_GoBackButton_pressed():
 	get_tree().change_scene("res://Scenes/MainMenu.tscn")
+
+
+func _on_JoinButton_pressed():
+	if _ip_input.text == "":
+		_ip_input.shake()
+	else:
+		pass
